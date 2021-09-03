e2b02213eccf10fd144568af52597df7163d36fe
Alex <abadragan@gmail.com>
2021-09-03 22:46:54 +0200

    first commit for the coffee machine program

---
+++ coffee_machine/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eiannone/keyboard"
+	"log"
+	"strconv"
+)
+
+func main() {
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	fmt.Println("MENU")
+	fmt.Println("----")
+	fmt.Println("1 - Cappucino")
+	fmt.Println("2 - Latte")
+	fmt.Println("3 - Americano")
+	fmt.Println("4 - Mocha")
+	fmt.Println("5 - Macchiato")
+	fmt.Println("6 - Espresso")
+	fmt.Println("Q - Quit the program")
+
+	for {
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		i, _ := strconv.Atoi(string(char))
+		t := fmt.Sprintf("You chose %d", i)
+		fmt.Println(t)
+
+		if char == 'q' || char == 'Q' {
+			break
+		}
+	}
+
+	fmt.Println("Program exiting...")
+}
